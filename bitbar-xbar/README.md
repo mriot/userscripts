a95f5371f8419a6cc2516e1e94c12a4da10e148d
Markus Kremer <mkremer@seibert-media.net>
2021-05-16 20:53:30 +0200

    add xbar plugin

---
+++ bitbar-xbar/README.md
@@ -0,0 +1,12 @@
+# Scripts for [xbar](https://github.com/matryer/xbar) <small>(the BitBar successor)</small>
+
+## Installing
+
+- Place scripts in `Library/ApplicationSupport/xbar/plugins/`
+- Make them executable: `chmod +x scriptname.py`
+- (Python) Make sure to install the packages used in the script
+  - e.g. `pip3 install requests`
+
+## Community plugins
+
+<https://github.com/matryer/xbar-plugins>
---
+++ bitbar-xbar/rki.1h.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/local/bin/python3
+
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html")
+soup = BeautifulSoup(page.content, 'html.parser')
+region = soup.find("td", text="Hessen").find_next_siblings()
+# just replace "Hessen" with your federal state. E.g. "Saarland"
+
+print(region[3].text)
+print(f"---") # create sub-menu
+print(f"An­zahl: {region[0].text}")
+print(f"Dif­fe­renz zum Vor­tag: {region[1].text}")
+print(f"Fälle in den letzten 7 Tagen: {region[2].text}")
+print(f"7-Tage-Inzi­denz: {region[3].text}")
+print(f"Todes­fälle: {region[4].text}")
