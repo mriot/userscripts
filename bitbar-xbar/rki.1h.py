467c841c0205ae017be4e911e1c07bf04be93830
Markus Kremer <mkremer@seibert-media.net>
2021-05-17 11:05:09 +0200

    add "open website" menu item

--- bitbar-xbar/rki.1h.py
+++ bitbar-xbar/rki.1h.py
@@ -4,7 +4,8 @@ from time import localtime, strftime
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html")
+url = "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html"
+page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 region = soup.find("td", text="Hessen").find_next_siblings()
 # just replace "Hessen" with your federal state. E.g. "Saarland"
@@ -18,3 +19,5 @@ print(f"7-Tage-Inzi­denz: {region[3].text}")
 print(f"Todes­fälle: {region[4].text}")
 print(f"---")
 print("Last update:", strftime("%Y-%m-%d %H:%M:%S", localtime()))
+print(f"---")
+print(f"Open RKI website | href={url}")
