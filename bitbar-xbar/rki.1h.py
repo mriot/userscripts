d499be77a0eb4d1f1f2c4121c8ba71cbcf8880ab
Markus Kremer <mkremer@seibert-media.net>
2021-06-08 00:06:05 +0200

    add an emoji as icon

--- bitbar-xbar/rki.1h.py
+++ bitbar-xbar/rki.1h.py
@@ -10,7 +10,7 @@ soup = BeautifulSoup(page.content, 'html.parser')
 region = soup.find("td", text="Hessen").find_next_siblings()
 # just replace "Hessen" with your federal state. E.g. "Saarland"
 
-print(region[3].text)
+print("🦠", region[3].text)
 print(f"---") # create sub-menu
 print(f"An­zahl: {region[0].text}")
 print(f"Dif­fe­renz zum Vor­tag: {region[1].text}")
