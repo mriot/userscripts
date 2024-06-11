b65fa0434b7d7eed54622ae84585dd9928cd71ee
Markus <kremer.markus1996@gmail.com>
2024-06-11 03:23:28 +0200

    condense; add container element check; minor stuff

--- node-ready.js
+++ node-ready.js
@@ -2,29 +2,28 @@
  * Wait for a node to be ready in the DOM.
  * Can be used with a callback or as a promise.
  *
- * @param {string} selector - The CSS selector to watch for.
- * @param {string} container - The CSS selector of the container to watch.
+ * @param {string} targetSelector - The CSS selector to watch for.
+ * @param {string} containerSelector - The CSS selector of the container to watch.
  * @param {function} [callback] - The callback to execute when the node is ready. If not provided, a promise is returned.
  * @param {boolean} [keepWatching=false] - Whether to keep watching for new nodes. Can only be used with a callback.
  */
-const nodeReady = (selector, container, callback, keepWatching = false) => {
+const nodeReady = (targetSelector, containerSelector, callback, keepWatching = false) => {
     const watcher = (resolve) => {
-        const element = document.querySelector(selector);
-        if (element) {
-            callback ? callback(element) : resolve(element);
-            return;
-        }
+        const container = document.querySelector(containerSelector);
+        const element = document.querySelector(targetSelector);
+        if (element) return callback ? callback(element) : resolve(element);
+        if (!container) return console.error(`nodeReady(): Container '${containerSelector}' not found.`);
         new MutationObserver((mutations, observer) => {
             for (const mutation of mutations) {
                 for (const node of mutation.addedNodes) {
-                    if (node.matches(selector)) {
+                    if (node.matches(targetSelector)) {
                         callback ? callback(node) : resolve(node);
                         if (!keepWatching) observer.disconnect();
                         return;
                     }
                 }
             }
-        }).observe(document.querySelector(container), { childList: true, subtree: true });
+        }).observe(container, { childList: true, subtree: true });
     };
     return callback ? watcher() : new Promise(watcher);
 };
